ad108caee85e0f5647e1a365d5b298f48a4c2807
MARIAJULIASOLER <JULIASOLERNUNES@YMAIL.COM>
2021-03-25 11:47:01 +0100

    this is real

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Link, NavLink} from "react-router-dom"
+
+export default function NavBar () {
+    return (
+        <div>
+            <Link style={{marginRight:'10px'}} to="/" exact> Home</Link>
+            <Link style={{marginRight:'10px'}} to="/aboutus" exact> About Us</Link>
+            <Link style={{marginRight:'10px'}} to="time" exact> Oppening hours</Link>
+        </div>
+    )
+}
---
+++ src/pages/Time.js
@@ -0,0 +1,8 @@
+export default function Time () {
+    return(
+        <div>
+            <h1>
+             time
+            </h1>
+        </div>)
+}
